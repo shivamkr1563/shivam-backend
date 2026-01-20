8900e2f421d50083c1ae4449994dc04465ee2ab3
Shivam Kumar <shivamkr1562@gmail.com>
2026-01-20 21:59:08 +0530

    git setup configured

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -3,7 +3,7 @@ class ApiError extends Error {
         statusCode,
         message="Something went wrong",
         errors = [],
-        statck = ""
+        stack = ""
 
     ){
         super(message)
@@ -13,8 +13,8 @@ class ApiError extends Error {
         this.success = false
         this.errors = errors
 
-        if(statck) {
-            this.statck = this.statck
+        if(stack) {
+            this.stack = this.stack
         }else{
             Error.captureStackTrace(this,this.constructor)
         }
